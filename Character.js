5037f586435188bfe8a4de8b9a7a9472eb95e82f
socalled kubick <socalledsound@gmail.com>
2020-10-03 09:15:34 -0700

    character class

---
+++ Character.js
@@ -0,0 +1,40 @@
+class Character {
+    constructor({x, y, w, h, img, speed}){
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.img = img;
+        this.speed = speed;
+    }
+
+    display(){
+        image(this.img, this.x, this.y, this.w, this.h)
+    }
+
+    move(direction, amount){
+        if(direction === 'left'){
+            this.x -= amount;
+        } else if(direction === 'right'){
+            this.x += amount;
+        }
+
+    }
+
+    constantMove(){
+        if(this.x < 0  || this.x > canvasWidth - this.w){
+            this.speed *= -1;
+        }
+        this.x += this.speed;
+    }
+
+}
+
+
+// class SmilingCharacter extends Character {
+//     constructor({x, y, w, h, img, speed}){
+//         super({x, y, w, h, img, speed})
+//         this.h = h * 2.5;
+//         this.w = w * 2;
+//     }
+// }
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
-let myTotoroImage;
+let smilingTotoroImage, confusedTotoroImage, pikachuImage;
+let smilingCharacter, confusedCharacter, pikachuCharacter;
 const imgSize = 200;
+const canvasWidth = 600, canvasHeight = 600;
 
 const ground = {
     y: 550,
@@ -40,37 +42,36 @@ const imageHeight = 100;
 function preload(){
     
     // myTotoroImage = loadImage('assets/totoro-use.png');
-    totoroObject.image = loadImage('assets/totoro-use.png');
-    smilingTotoroObject.image = loadImage('assets/smiling-totoro.png');
-    pikachuObject.image = loadImage('assets/pikachu.png');
+    totoroObject.img = loadImage('assets/totoro-use.png');
+    smilingTotoroObject.img = loadImage('assets/smiling-totoro.png');
+    pikachuObject.img = loadImage('assets/pikachu.png');
 
 }
 function setup(){
-    createCanvas( 600, 600);
+    createCanvas( canvasWidth, canvasHeight);
+
+    
+    smilingCharacter = new Character(smilingTotoroObject);
+    confusedCharacter = new Character(totoroObject);
+    pikachuCharacter = new Character(pikachuObject);
+
+
+    // smilingCharacter = new SmilingCharacter(smilingTotoroObject);
+
+
 }
 
 function draw(){
     background(200,190,250);
     drawGround(ground);
     
-    if(totoroObject.x < 0  || totoroObject.x > width - totoroObject.w){
-        totoroObject.speed *= -1;
-    }
-    totoroObject.x += totoroObject.speed;
 
-    if(smilingTotoroObject.x < 0  || smilingTotoroObject.x > width - smilingTotoroObject.w){
-        smilingTotoroObject.speed *= -1;
-    }
-    smilingTotoroObject.x += smilingTotoroObject.speed;
-
-    if(pikachuObject.x < 0  || pikachuObject.x > width - pikachuObject.w){
-        pikachuObject.speed *= -1;
-    }
-    pikachuObject.x += pikachuObject.speed;
-
-    image(totoroObject.image, totoroObject.x, totoroObject.y, totoroObject.w, totoroObject.h);
-    image(smilingTotoroObject.image, smilingTotoroObject.x, smilingTotoroObject.y, smilingTotoroObject.w, smilingTotoroObject.h);
-    image(pikachuObject.image, pikachuObject.x, pikachuObject.y, pikachuObject.w, pikachuObject.h);
+    smilingCharacter.constantMove();
+    smilingCharacter.display();
+    confusedCharacter.constantMove();
+    confusedCharacter.display();
+    pikachuCharacter.constantMove();
+    pikachuCharacter.display();
 
 
 }
