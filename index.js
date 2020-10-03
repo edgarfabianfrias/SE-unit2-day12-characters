b3804297af06e8528c76bc85e49e1ee3bf8529f8
socalled kubick <socalledsound@gmail.com>
2020-10-02 21:02:34 -0700

    moving totoro

--- index.js
+++ index.js
@@ -1,21 +1,52 @@
 
 let myTotoroImage;
+const imgSize = 100;
+
+const ground = {
+    y: 550,
+    color: 'brown',
+}
+
+const totoroObject = { 
+    x: 0,
+    y: ground.y - imgSize,
+    w: imgSize,
+    h: imgSize,
+    speed: 10,
+
+}
+
+const imageHeight = 100;
+
 
 function preload(){
     
-    myTotoroImage = loadImage('assets/totoro-use.png');
+    // myTotoroImage = loadImage('assets/totoro-use.png');
+    totoroObject.image = loadImage('assets/totoro-use.png');
+
 }
 function setup(){
     createCanvas( 600, 600);
 }
 
 function draw(){
+    background(200,190,250);
+    drawGround(ground);
+    
+    if(totoroObject.x < 0  || totoroObject.x > width - totoroObject.w){
+        totoroObject.speed *= -1;
+    }
+    totoroObject.x += totoroObject.speed;
 
-    image(myTotoroImage, 0, 0, 100, 100);
+    image(totoroObject.image, totoroObject.x, totoroObject.y, totoroObject.w, totoroObject.h);
 
 
 }
 
 
+function drawGround(ground){
+    fill(ground.color);
+    rect(0, ground.y, width, height - ground.y);
+}
 
 
