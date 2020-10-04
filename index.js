496cf3fd8a703cbb7ee7ca688649d15402790caa
socalled kubick <socalledsound@gmail.com>
2020-10-03 17:35:08 -0700

    edits on branch 2

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-
+//variable for our image -- decalre it globally so we can access it in both the preload and draw functions.
 let myTotoroImage;
 const imgHeight = 100;
+
+const canvasWidth = 600, canvasHeight = 600;
+const horizon = 500;
 const ground = {
-    y: 550,
+    x: 0,
+    y: horizon,
+    w: canvasWidth,
+    h: canvasHeight - horizon,
     color: 'brown',
-}
 
-
-const imageHeight = 100;
+};
 
 
 function preload(){
-    
     myTotoroImage = loadImage('assets/totoro-use.png');
 }
 function setup(){
-    createCanvas( 600, 600);
+    createCanvas( canvasWidth, canvasHeight);
 }
 
 function draw(){
@@ -23,13 +26,11 @@ function draw(){
     drawGround(ground);
     image(myTotoroImage, 0, ground.y - imgHeight, 100, imgHeight);
 
-
 }
-
 
 function drawGround(ground){
     fill(ground.color);
-    rect(0, ground.y, width, height - ground.y);
+    rect(ground.x, ground.y, ground.w, ground.h);
 }
 
 
